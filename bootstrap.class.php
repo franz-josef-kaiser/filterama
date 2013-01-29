dfe4d0a32893ad85eadb63312a43a5fcf33a57be
Kaiser Franz Josef <wecodemore@gmail.com>
2013-01-29 08:24:23 +0100

    better loading logic

---
+++ bootstrap.class.php
@@ -0,0 +1,39 @@
+<?php
+defined( 'ABSPATH' ) OR exit;
+/**
+ * Plugin Name:  (WCM) Filterama
+ * Plugin URI:   http://example.com
+ * Description:  Adds one taxonomy filter/drop-down/select box for each taxonomy attached to a (custom) post types list in the admin post list page.
+ * Version:      0.3.3
+ * Author:       Franz Josef Kaiser <wecodemore@gmail.com>
+ * Author URI:   http://example.com
+ * Contributors: userabuser, kai-ser
+ * License:      MIT
+ */
+
+add_action( 'plugins_loaded', array( 'WCMF_bootstrap', 'init' ), 5 );
+/**
+ * @package    WCM Filterama
+ * @subpackage Bootstrap
+ */
+class WCMF_bootstrap
+{
+	private static $instance;
+
+	static function init()
+	{
+		null === self :: $instance AND self :: $instance = new self;
+		return self :: $instance;
+	}
+
+	public function __construct()
+	{
+		add_action( 'load-edit.php', array( $this, 'load' ), 0 );
+	}
+
+	public function load()
+	{
+		foreach ( glob( plugin_dir_path( __FILE__ ).'inc/*.php' ) as $file )
+			require_once $file;
+	}
+}

25571c6f713cd7f6707733fee49ab26c9927f01c
Kaiser Franz Josef <wecodemore@gmail.com>
2013-01-29 09:03:43 +0100

    added post status property

--- inc/base.abstract.php
+++ inc/base.abstract.php
@@ -11,15 +11,21 @@ abstract class WCMF_base
 
 	public $taxonomies;
 
+	public $post_status;
+
 	public function __construct()
 	{
-		$this->post_type  = get_current_screen()->post_type;
-		$this->taxonomies = array_diff(
+		null === $this->post_type   AND $this->post_type  = get_current_screen()->post_type;
+		null === $this->taxonomies AND $this->taxonomies = array_diff(
 			 get_object_taxonomies( $this->post_type )
 			,get_taxonomies( array(
 				'show_admin_column' => false
 			 ) )
 		);
+		null === $this->post_status AND $this->post_status = get_post_stati( array(
+			 'show_in_admin_status_list' => true
+			,'show_in_admin_all_list'    => true
+		) );
 
 		add_action( current_filter(), array( $this, 'setup_actions' ), 20 );
 		add_action( 'restrict_manage_posts', array( $this, 'get_markup' ) );
